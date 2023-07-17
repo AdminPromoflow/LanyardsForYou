943d5697dd33791496dd8d68b7a8262fd6f568fc
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2023-07-17 14:03:27 -0500

    Update index.php

--- Pages/Index/index.php
+++ Pages/Index/index.php
@@ -17,7 +17,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
   </head>
   <body>
-    <script type="text/javascript"  src="../../Pages/Libraries/jquery-3.5.0.min.js"></script>
+    <script type="text/javascript"  src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
 
     <style media="screen">
       body{
