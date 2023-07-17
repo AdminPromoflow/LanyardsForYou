e4f764ebd83a197fcb8fa4197a11cc6a4732e371
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2023-07-17 16:45:29 -0500

    Update Login.php

--- Pages/General/Login/Login.php
+++ Pages/General/Login/Login.php
@@ -14,9 +14,9 @@
   }
   .containerLogin{
     position: relative;
-    min-height: 430px;
+    min-height: 450px;
     min-width: 300px;
-    height: 430px;
+    height: 450px;
     width: 400px;
     border-radius: 5px;
     padding: 30px;
