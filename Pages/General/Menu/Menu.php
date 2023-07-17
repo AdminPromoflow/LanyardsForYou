a8d289e441f78baab55afd916674fbdc73ccaf33
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2023-07-17 16:03:52 -0500

    Update Menu.php

--- Pages/General/Menu/Menu.php
+++ Pages/General/Menu/Menu.php
@@ -205,7 +205,7 @@
     </div>
   </a>
   <a href="">
-    <div id="openLogin2" class="subitemMenu">
+    <div id="openLogin" class="subitemMenu">
       <h3>Login</h3>
     </div>
   </a>
@@ -285,7 +285,7 @@
 
 
 
-   var openLogin = document.getElementById("openLogin2");
+   var openLogin = document.getElementById("openLogin");
    openLogin.addEventListener("click",function(){
    bgLogin.display = "flex";
    });
