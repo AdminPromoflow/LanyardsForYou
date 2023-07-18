e4abf10efb02cfb1bb64d4a95081e7038ca5ec39
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2023-07-18 14:10:40 -0500

    Update Register.php

--- Pages/General/Register/Register.php
+++ Pages/General/Register/Register.php
@@ -53,6 +53,8 @@
       height: calc(100% - 80px);
       width: 100%;
       background-color: #e0e0e0;
+      border-top-left-radius: 5px;
+      border-top-right-radius: 5px;
     }
     .inputRegister{
       position: relative;
